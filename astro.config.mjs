f5d983042994d49bf1304618ed2735dcef219364
Sudário Roberto <ssilvaju@umn.edu>
2025-04-04 11:06:56 -0500

    Upload all images and assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel'; // ✅ current import
 
 export default defineConfig({
+  output: 'server', // Required for SSR
+  adapter: vercel(),
   integrations: [tailwind()]
 });
